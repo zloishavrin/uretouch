940e9f54e34be36aa018e166134b5222e911ce78
Evgeny Meltsaykin <mel.ea@skbkontur.ru>
2024-05-31 22:15:53 +0500

    Initial mobile application

---
+++ mobile/composeApp/src/commonMain/kotlin/com/uretouch/app/App.kt
@@ -0,0 +1,26 @@
+package com.uretouch.app
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import uretouch.composeapp.generated.resources.*
+import com.uretouch.app.theme.AppTheme
+import com.uretouch.app.theme.LocalThemeIsDark
+import org.jetbrains.compose.resources.Font
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+internal fun App() = AppTheme {
+
+}
+
+internal expect fun openUrl(url: String?)
---
+++ mobile/settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "URetouch"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
